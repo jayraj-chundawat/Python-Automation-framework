14003107d6eadd9ef6ac69f9244994a6d198eac6
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-16 23:04:58 +0530

    added logger name and info in logger class

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,10 @@
+import logging
+
+class Logger:
+    def __init__(self, name="AutomationFramework"):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+
+    def get_logger(self):
+        return self.logger
 
