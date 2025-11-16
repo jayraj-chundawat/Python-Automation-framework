d64f9581007582fd0c84f72b7e761a042730b531
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-16 23:22:19 +0530

    Added console handler and log formatter to Logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,18 @@
 import logging
 
 class Logger:
+
     def __init__(self, name="AutomationFramework"):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
 
+        if not self.logger.handlers:  # prevent duplicate handlers
+            console_handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
+
     def get_logger(self):
         return self.logger
+
 
