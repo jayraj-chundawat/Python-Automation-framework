c8d60a6354c4c6a79d0123a7af8ba418335f11e6
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-17 21:47:30 +0530

    Add Getter for Env

--- config/config_manager.py
+++ config/config_manager.py
@@ -5,6 +5,7 @@ class ConfigManager:
 
     def __init__(self, config_path="config/config.json"):
         config_file = os.path.abspath(config_path)
+
         with open(config_file, "r") as file:
             self.config = json.load(file)
 
@@ -12,5 +13,9 @@ class ConfigManager:
 
     def get_base_url(self):
         return self.config[self.env]["base_url"]
+
+    def get_env(self):
+        return self.env
+
 
 
