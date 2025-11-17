4b0ad89596adaf002f9dd7d9a09488a48d809c54
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-17 21:34:16 +0530

    created ConfigManager class and Implemented loading of config.json

--- config/config_manager.py
+++ config/config_manager.py
@@ -1,1 +1,10 @@
+import json
+import os
+
+class ConfigManager:
+
+    def __init__(self, config_path="config/config.json"):
+        config_file = os.path.abspath(config_path)
+        with open(config_file, "r") as file:
+            self.config = json.load(file)
 
