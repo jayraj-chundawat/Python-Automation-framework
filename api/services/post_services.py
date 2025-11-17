0d677c136123db2faef1753cce8ace9b63bc09db
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-17 22:17:16 +0530

    added handler response code

--- api/services/post_services.py
+++ api/services/post_services.py
@@ -1,24 +1,26 @@
 from api.services.client import APIClient
 from config.config_manager import ConfigManager
 
+
 class PostService(APIClient):
-    """Service class to manage post-related API calls"""
 
     def __init__(self):
         config = ConfigManager()
         base_url = config.get_base_url()
+
         super().__init__(base_url)
 
     def get_post(self, post_id):
-        return self.get(f"/posts/{post_id}")
+        return self._handle_response(self.get(f"/posts/{post_id}"))
 
     def create_post(self, payload):
-        return self.post("/posts", json_data=payload)
-        
+        return self._handle_response(self.post("/posts", json_data=payload))
+
     def update_post(self, post_id, payload):
-        return self.put(f"/posts/{post_id}", json_data=payload)
+        return self._handle_response(self.put(f"/posts/{post_id}", json_data=payload))
 
     def delete_post(self, post_id):
-        return self.delete(f"/posts/{post_id}")
+        return self._handle_response(self.delete(f"/posts/{post_id}"))
+
 
 
