3dcead57a3db218d6b6c309fa880d04753eedfd9
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-18 21:46:47 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,8 @@
+import pytest
+from api.services.post_services import PostService
+
+@pytest.fixture(scope="session")
+def post_service():
+    """Provides PostService instance for test functions"""
+    return PostService()
 
