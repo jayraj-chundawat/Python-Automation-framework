8e6559aa75a492e1bacb3a1d53761153ae4ee8b8
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-18 21:55:41 +0530

    Add Summary Metadata to Reports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import platform
 from api.services.post_services import PostService
 
 @pytest.fixture(scope="session")
@@ -6,7 +7,13 @@ def post_service():
     return PostService()
 
 def pytest_html_report_title(report):
-    """Customize the title of the HTML report"""
     report.title = "Python API Automation Test Report"
+
+def pytest_html_results_summary(prefix, summary, postfix):
+    """Add metadata or details to the summary section of the report"""
+    prefix.extend([f"Project Name: Python API Automation Framework"])
+    prefix.extend([f"Tester: Jay raj"])  # Replace as needed
+    prefix.extend([f"Platform: {platform.system()}"])
+
 
 
