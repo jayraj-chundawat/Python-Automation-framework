d9eba7c9d416c43a23737c0c5b1c6ab0f8199dd7
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-19 22:10:10 +0530

    Updated SchemaValidator to load schema from file path

--- utils/schema_validator.py
+++ utils/schema_validator.py
@@ -1,13 +1,19 @@
+import json
 from jsonschema import validate, ValidationError
+import os
 
 class SchemaValidator:
 
     @staticmethod
-    def validate_response(schema, response_data):
+    def validate_response(schema_path, response_data):
         try:
+            with open(schema_path, 'r') as schema_file:
+                schema = json.load(schema_file)
+
             validate(instance=response_data, schema=schema)
             return True, None
 
         except ValidationError as ve:
             return False, f"Schema validation error: {ve.message}"
+
 
