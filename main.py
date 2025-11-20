d9591c672a48af475c77e1239b1722db10dab19c
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-20 21:09:29 +0530

    Added basic GET request test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from api.services.post_services import PostService
+
+if __name__ == "__main__":
+    service = PostService()
+    status, response = service.get_post(1)
+
+    print("GET Status:", status)
+    print("GET Response:", response)
 
