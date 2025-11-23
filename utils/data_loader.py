630be8ae5706f99fd57f8bb2f66ec82e32545a3c
jayraj-chundawat <jayraj.chundawat01@gmail.com>
2025-11-23 20:18:27 +0530

    Updated data_loader.py for csv file handling support

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,4 +1,5 @@
 import json
+import csv
 import os
 
 class DataLoader:
@@ -8,3 +9,17 @@ class DataLoader:
         abs_path = os.path.abspath(file_path)
         with open(abs_path, "r") as file:
             return json.load(file)
+
+    @staticmethod
+    def load_csv(file_path):
+        abs_path = os.path.abspath(file_path)
+        data_list = []
+
+        with open(abs_path, "r", newline='', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                # Convert numbers automatically
+                row["userId"] = int(row["userId"])
+                data_list.append(row)
+
+        return data_list
